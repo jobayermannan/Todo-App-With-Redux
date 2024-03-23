34e647f2f2c57986013cbb7211c02f9ed31a5441
jobayermannan <jobayermannan777@gmail.com>
2024-03-23 19:05:17 +0600

    initial Tailwind,Shadcn setup done

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import { Button } from "./components/ui/button";
 
 function App() {
   return (
     <>
       <h1 className="bg-green-500">My Todo</h1>
+      <Button>My Todo</Button>
     </>
   );
 }
